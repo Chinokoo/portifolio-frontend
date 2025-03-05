2e1d5d0a4c2062716ce7c9db6327e5b35184bcbb
Peter <peterchinokoo@gmail.com>
2025-03-05 18:37:36 +0200

    running build command

--- src/config/axios/axios.js
+++ src/config/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001/api", //Todo: add production url
+  baseURL: "https://portifolio-backend-pppp.onrender.com/api",
   withCredentials: true,
   credentials: "include",
   headers: {
