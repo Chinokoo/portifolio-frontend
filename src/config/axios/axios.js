922f21e532a86f3732696111aa959cdc444772f2
Peter <peterchinokoo@gmail.com>
2025-03-05 22:40:10 +0200

    add url production url

--- src/config/axios/axios.js
+++ src/config/axios/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    //"https://portifolio-backend-pppp.onrender.com/api",
-    "http://localhost:3001/api",
+  baseURL: "https://portifolio-backend-pppp.onrender.com/api",
+  // "http://localhost:3001/api",
   withCredentials: true,
   credentials: "include",
   headers: {
